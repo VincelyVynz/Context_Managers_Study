3e940eb8e9ed52d349b0052c1bf336d7359ba425
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-08-29 17:17:20 +0530

    Add error handling to FileContextManager.

--- file_context_manager.py
+++ file_context_manager.py
@@ -4,16 +4,30 @@ class FileContextManager():
         self.mode = mode
 
     def __enter__(self):
-        self.file = open(self.filename, self.mode)
-        return self.file
+        try:
+            self.file = open(self.filename, self.mode)
+            return self.file
+
+        except FileNotFoundError:
+            print("File not found")
+            return None
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        self.file.close()
+        try:
+            self.file.close()
+        except AttributeError:
+            pass
+        if exc_type:
+            print(f"An error occurred: {exc_value}")
+            return True
+        return None
 
 
-with FileContextManager("testfile.txt", "r") as file:
-    content = file.read()
-    print(content)
+#Testing with a non existent file:
+with FileContextManager("testf.txt", "r") as file:
+    if file:
+        content = file.read()
+        print(content)
 
 with FileContextManager("testfile.txt", "a") as file:
     new_content = file.write("\n Add a new line")
