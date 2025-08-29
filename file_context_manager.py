8f5cac3874de397978b01ba1d6aa425dd8381915
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-08-29 16:16:11 +0530

    Create a custom file context manager

---
+++ file_context_manager.py
@@ -0,0 +1,16 @@
+class File_Context_Manager():
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.file.close()
+
+
+with File_Context_Manager("testfile.txt", "r") as file:
+    content = file.read()
+    print(content)
